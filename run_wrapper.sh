6f74f5134d5524e65504e28677540cea0b23ccb0
frank@tscc <zhangc518@gmail.com>
2017-12-21 14:02:22 -0800

    fix fastq name bug in wrapper

--- run_wrapper.sh
+++ run_wrapper.sh
@@ -11,4 +11,4 @@ cur_lane=${lanes[PBS_ARRAYID]}
 outdir="${fastqdir}${cur_lane}"
 mkdir -p $outdir
 
-debarcode.py --i1 $fastqdir${cur_lane}_I1.fastq.gz --i2 $fastqdir${cur_lane}_I2.fastq.gz --r1 $fastqdir${cur_lane}_R1.fastq.gz  --r2 $fastqdir${cur_lane}_R2.fastq.gz --barcode /home/zhc268/data/software/snATAC_qc/barcodes/barcodes.txt -o $outdir
+debarcode.py --i1 $fastqdir${cur_lane}_I1_001.fastq.gz --i2 $fastqdir${cur_lane}_I2_001.fastq.gz --r1 $fastqdir${cur_lane}_R1_001.fastq.gz  --r2 $fastqdir${cur_lane}_R2_001.fastq.gz --barcode /home/zhc268/data/software/snATAC_qc/barcodes/barcodes.txt -o $outdir
